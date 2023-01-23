f9f287b10b2abc64f4086848eefa2bd981ca49e1
williamcerdelli <cerdelliwilliam@gmail.com>
2023-01-23 15:59:14 +1100

    fixed linting

--- COMPONENTS/src/working.py
+++ COMPONENTS/src/working.py
@@ -1,14 +1,10 @@
 # native libraries
 import os
-import requests
 import json
-from pprint import pprint
 
 # third party modules
 from dotenv import load_dotenv
-import os
 import openai
-import pandas as pd
 from notion_client import Client
 
 load_dotenv()
@@ -28,35 +24,37 @@ def write_text(client, page_id, text, type):
         block_id=page_id,
         children=[
             {
-            "object": "block",
-            "type": type,
-            type: {
+                "object": "block",
+                "type": type,
+                type: {
                     "rich_text": [
                         {
-                        "type": "text",
-                        "text": {
-                            "content": text
+                            "type": "text",
+                            "text": {
+                                "content": text
                             }
                         }
                     ]
 
                 }
             }
-        
+
         ]
     )
-    
+
 
 def read_text(client, page_id):
     response = client.blocks.children.list(block_id=page_id)
     return response['results']
 
+
 def read_df(client, database_id):
     response = client.databases.query(database_id=database_id)
     return response['results']
 
+
 def ai_ready(page):
-   return page['properties']['Status']['status']['name'] == 'Ready'
+    return page['properties']['Status']['status']['name'] == 'Ready'
 
 
 def create_simple_blocks_from_content(client, content):
@@ -73,30 +71,31 @@ def create_simple_blocks_from_content(client, content):
         if not rich_text:
             return page_simple_blocks
 
-
         simple_block = {
-#             'id': block_id,
+            # 'id': block_id,
             'type': block_type,
             'text': rich_text[0]['plain_text']
         }
 
         if has_children:
             nested_children = read_text(client, block_id)
-            simple_block['children'] = create_simple_blocks_from_content(client, nested_children)
+            simple_block['children'] = create_simple_blocks_from_content(
+                client,
+                nested_children
+                )
 
         page_simple_blocks.append(simple_block)
-
 
     return page_simple_blocks
 
 
-def read_page(client,notion_page_id):
+def read_page(client, notion_page_id):
     content = read_text(client, notion_page_id)
 
     simple_blocks = create_simple_blocks_from_content(client, content)
 
     return simple_blocks
-    
+
 
 def ai_response(ai_prompt):
     response = openai.Completion.create(
@@ -108,43 +107,42 @@ def ai_response(ai_prompt):
       frequency_penalty=0.5,
       presence_penalty=0
     )
-    
+
     return response["choices"][0]["text"].strip("/n")
 
 
 def update_status(client, page_id):
     client.pages.update(
-        page_id = page_id,
-        properties = {
-     'Status': {
-    'status': {
-     'name': 'Done'}},
-    }
-        
+        page_id=page_id,
+        properties={
+            'Status': {
+                'status': {
+                    'name': 'Done'}},
+                    }
+
     )
 
 
-def update_page(client,df):
+def update_page(client, df):
     for page in df:
         if ai_ready(page):
             ai_prompt = read_page(client, page["id"])
-            
+
             response = ai_response(ai_prompt)
 #             response = "Hello World"
-            
+
             write_text(client, page["id"], response, "paragraph")
-            
+
             update_status(client, page["id"])
-            
-    
+
+
 def main():
     client = Client(auth=token_notion)
-    
+
     df = read_df(client, database_id)
-    
-    update_page(client,df)
-    
+
+    update_page(client, df)
+
 
 if __name__ == '__main__':
     main()
-    
